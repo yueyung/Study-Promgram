239500c09174183828a60583582b705bfd835bf5
yueyung <20540761+yueyung@users.noreply.github.com>
2022-10-13 08:35:55 +0800

    Update homework1.py
    fix err: str + num

--- Python3/Day1/homework1.py
+++ Python3/Day1/homework1.py
@@ -6,7 +6,7 @@ name = input("请输入您的姓名：")
 num = input('请输入一个1~100的数中:')
 num = int(num)
 if num > random.randint(0,100):
-    print("恭喜您：" + name,"您输入的数字"+ num +"大于随机数")
+    print("恭喜您：" + name,"您输入的数字", num, "大于随机数")
 else:
     print("不能恭喜您了",name)
 
